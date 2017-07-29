a711a722f63a8567894deb687ac993cf01de64a3
Jonathan Whiting <jonathan@ignika.com>
2017-07-29 16:12:39 +0100

    some flight stuff

--- src/glider.c
+++ src/glider.c
@@ -1,36 +1,60 @@
 #include "glider.h"
 
 #include <whitgl/input.h>
+#include <whitgl/logging.h>
+
+
+	// more realistic maths that I couldn't get feeling right
+	// whitgl_fmat rot = whitgl_quat_to_fmat(glider.facing);
+	// whitgl_fvec3 forward = {0,1,0};
+	// whitgl_fvec3 up = {0,0,1};
+	// forward = whitgl_fvec3_apply_fmat(forward, rot);
+	// up = whitgl_fvec3_apply_fmat(up, rot);
+
+	// whitgl_float direction_dot = whitgl_fvec3_dot(whitgl_fvec3_normalize(glider.speed), forward);
+	// WHITGL_LOG("direction_dot %.2f", direction_dot);
+
+	// whitgl_float speed_mag = whitgl_fvec3_magnitude(glider.speed);
+	// whitgl_float drag_factor = 0.0001/direction_dot;
+	// whitgl_fvec3 drag = whitgl_fvec3_scale_val(forward, -drag_factor*speed_mag*speed_mag);
+	// whitgl_float lift_factor = 0.0007/direction_dot;
+	// whitgl_fvec3 lift = whitgl_fvec3_scale_val(up, lift_factor*speed_mag*speed_mag);
+	// whitgl_fvec3 gravity = {0,0,-9.8/6000};
+	// glider.speed = whitgl_fvec3_add(glider.speed, drag);
+	// glider.speed = whitgl_fvec3_add(glider.speed, lift);
+	// glider.speed = whitgl_fvec3_add(glider.speed, gravity);
 
 ld39_glider ld39_glider_update(ld39_glider glider)
 {
-	glider.pos = whitgl_fvec3_add(glider.pos, glider.speed);
+	glider.pos = whitgl_fvec3_add(glider.pos, whitgl_fvec3_scale_val(glider.speed, 1.0/60));
 
 	whitgl_fvec joystick = whitgl_input_joystick();
 
-	whitgl_fvec3 forward = {0,1,0};
-	whitgl_fvec3 side = {0,0,1};
-	// whitgl_fmat rot = whitgl_quat_to_fmat(glider.facing);
-	// forward = whitgl_fvec3_apply_fmat(forward, rot);
-	// side = whitgl_fvec3_apply_fmat(side, rot);
-
-			// whitgl_fvec3 u = {0,-1,0};
-			// whitgl_fvec3 r = {1,0,0};
-			// whitgl_fvec3 f = {0,0,1};
-			// whitgl_fvec target_jrot = {joy2.x*joy2.x, joy2.y*joy2.y};
-			// if(joy2.x < 0) target_jrot.x *= -1;
-			// if(joy2.y < 0) target_jrot.y *= -1;
-			// jrot = whitgl_fvec_interpolate(jrot, target_jrot, 0.2);
-			// orientation = whitgl_quat_multiply(whitgl_quat_rotate(jrot.y/48, f), orientation);
-			// orientation = whitgl_quat_multiply(whitgl_quat_rotate(jrot.x/48, u), orientation);
-	whitgl_quat roll = whitgl_quat_rotate(-joystick.x/64, forward);
-	whitgl_quat pitch = whitgl_quat_rotate(joystick.y/64, side);
+	whitgl_fvec3 roll_axis = {0,1,0};
+	whitgl_fvec3 pitch_axis = {0,0,1};
+	whitgl_quat roll = whitgl_quat_rotate(-joystick.x/64, roll_axis);
+	whitgl_quat pitch = whitgl_quat_rotate(joystick.y/64, pitch_axis);
 	glider.facing = whitgl_quat_multiply(pitch, glider.facing);
 	glider.facing = whitgl_quat_multiply(roll, glider.facing);
 
 
 	whitgl_fmat rot = whitgl_quat_to_fmat(glider.facing);
-	glider.speed = whitgl_fvec3_scale_val(whitgl_fvec3_apply_fmat(forward, rot),0.1);
+	whitgl_fvec3 forward = {0,1,0};
+	whitgl_fvec3 up = {0,0,1};
+
+	forward = whitgl_fvec3_apply_fmat(forward, rot);
+	up = whitgl_fvec3_apply_fmat(up, rot);
+	glider.speed = whitgl_fvec3_interpolate(glider.speed, whitgl_fvec3_scale_val(forward, glider.forward_speed), 0.02);
+
+	whitgl_fvec3 gravity = {0,0,-1};
+	whitgl_float gravity_dot = whitgl_fvec3_dot(glider.speed, gravity);
+	glider.forward_speed = glider.forward_speed+gravity_dot/100;
+	glider.forward_speed = glider.forward_speed*0.999;
+
+	if(whitgl_input_held(WHITGL_INPUT_A))
+		glider.forward_speed += 0.1;
+
+
 
 
 	return glider;
--- src/glider.h
+++ src/glider.h
@@ -8,9 +8,10 @@ typedef struct
 	whitgl_fvec3 pos;
 	whitgl_quat facing;
 	whitgl_fvec3 speed;
+	whitgl_float forward_speed;
 } ld39_glider;
 
-static const ld39_glider ld39_glider_zero = {{64,64,12},{0,0,0,1},{0,-0.01,0}};
+static const ld39_glider ld39_glider_zero = {{64,64,12},{0,0,0,1},{0,-1,0},1};
 ld39_glider ld39_glider_update(ld39_glider glider);
 whitgl_fmat ld39_glider_onboard_camera(ld39_glider glider);
 
